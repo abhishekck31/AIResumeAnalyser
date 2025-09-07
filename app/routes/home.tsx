e8126f13eda2b634ecf22ddfa487c236e029c4f4
abhishekck31 <abhishekkannur31@gmail.com>
2025-09-07 08:58:36 +0530

    Implemented Home Screen , Navbar & ResumeCards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import type { ReactNode } from "react";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  function callbackfn(value: Resume, index: number, array: Resume[]): ReactNode {
+    throw new Error("Function not implemented.");
+  }
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section py-16">
+      <div className="page-heading">
+        <h1>Track Your Applications and Resume Ratings</h1>
+        <h2>Review your submissions and Check AI-Powered feedback.</h2>
+      </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>
 }
